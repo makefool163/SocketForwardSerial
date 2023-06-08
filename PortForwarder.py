e329d7a2f0178bca9a0d14cf81c220b1b4140c51
makefool <makefool@163.com>
2023-06-08 23:39:07 +0800

    modified:   PortForwarder.py

--- PortForwarder.py
+++ PortForwarder.py
@@ -11,7 +11,7 @@ def closed_callback():
     print("called back")
 
 
-def forward(source, dest, cb=lambda: None):
+def c_2_s_forward(source, dest, cb=lambda: None):
     """Forwards bytes unidirectionally from source to dest"""
     while True:
         d = source.recv(32384)
@@ -19,11 +19,22 @@ def forward(source, dest, cb=lambda: None):
             cb()
             break
         dest.sendall(d)
+        print ("c_s: \t",len(d), end="\t\t")
+
+def s_2_c_forward(source, dest, cb=lambda: None):
+    """Forwards bytes unidirectionally from source to dest"""
+    while True:
+        d = source.recv(32384)
+        if d == '':
+            cb()
+            break
+        dest.sendall(d)
+        print ("s_c: \t",len(d))
 
 listener = eventlet.listen(('localhost', 13333))
 while True:
     client, addr = listener.accept()
     server = eventlet.connect(('localhost', 22))
     # two unidirectional forwarders make a bidirectional one
-    eventlet.spawn_n(forward, client, server, closed_callback)
-    eventlet.spawn_n(forward, server, client)
+    eventlet.spawn_n(c_2_s_forward, client, server, closed_callback)
+    eventlet.spawn_n(s_2_c_forward, server, client)
